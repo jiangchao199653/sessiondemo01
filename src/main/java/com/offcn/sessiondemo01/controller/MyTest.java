ab56f56615ba3882e190b82562575e92529b3919
offcn-java <83850151@qq.com>
2020-12-17 18:03:59 +0800

    123

--- src/main/java/com/offcn/sessiondemo01/controller/MyTest.java
+++ src/main/java/com/offcn/sessiondemo01/controller/MyTest.java
@@ -3,5 +3,6 @@ package com.offcn.sessiondemo01.controller;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(444444);
     }
 }
